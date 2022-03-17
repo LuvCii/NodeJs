8989c6cde57fe4b565ebfc5c874aae9432aba98c
LuvCii <tranminhdungggg@gmail.com>
2022-03-17 11:17:58 +0700

    Day 5 Node

--- models/products.js
+++ models/products.js
@@ -1,4 +1,5 @@
-import mongoose, { Schema } from "mongoose";
+// import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, ObjectId } from "mongoose";
 const productSchema = new Schema({
     name: {
         type: String,
@@ -8,6 +9,14 @@ const productSchema = new Schema({
     price: {
         type: Number,
         required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category"
     }
 }, { timestamps: true });
 export default mongoose.model('Product', productSchema);
