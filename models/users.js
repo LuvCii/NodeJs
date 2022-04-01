ef6f93b02dee5ef438a4d9ccb49cd24939cf577f
LuvCii <tranminhdungggg@gmail.com>
2022-04-01 23:29:58 +0700

    add product by check role

--- models/users.js
+++ models/users.js
@@ -17,7 +17,7 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        minlength: 6,
+        minlength: 5,
         required: true
     },
     role: {
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from 'cors';
 import morgan from "morgan";
 import mongoose from "mongoose";
+import productRouter from "../routers/product";
+import authRouter from "../routers/auth";
 
 
 const app = express();
@@ -9,6 +11,10 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json())
 
+
+// Router
+app.use("/api", productRouter);
+app.use("/api", authRouter);
 
 
 // connect database
