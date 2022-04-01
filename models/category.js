55f918f5b75112ee96ee0816f8231cf431d00c6e
LuvCii <tranminhdungggg@gmail.com>
2022-04-01 22:18:29 +0700

    models of categories

---
+++ models/category.js
@@ -0,0 +1,8 @@
+import mongoose, { Schema } from "mongoose";
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+export default mongoose.model("Category", categorySchema)
